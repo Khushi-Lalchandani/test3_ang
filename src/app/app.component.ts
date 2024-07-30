c5a19f91bc2dfed15cc453c948e23b74ad22a4ea
Khushi-Lalchandani <140065096+Khushi-Lalchandani@users.noreply.github.com>
2024-07-30 11:55:45 +0530

    Initial update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'cmp-databinding-start';
+export class AppComponent implements OnInit {
+  showForm = false;
+  profileName: string = 'John';
+  email: string = 'john@gmail.com';
+  pNo: number = 1211232423;
+  age: number = 30;
+  evenNumbers: number[] = [];
+  oddNumbers: number[] = [];
+
+  constructor() {}
+  ngOnInit() {}
 }
